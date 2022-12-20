1450fb8a962346c58c63a960f33c21c17b2421dd
Jeseungyou <jeseungyou@pop-os.localdomain>
2022-12-20 05:55:26 -0500

    FEAT: Users Signup GET/POST API & Login POST API

---
+++ src/repositories/users.repository.js
@@ -0,0 +1,31 @@
+const { Op } = require("sequelize");
+
+class UsersRepository {
+    // instance variable of UsersRepository class
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields
+    #usersModel;
+    constructor (UsersModel) {
+        this.#usersModel = UsersModel;
+    }
+
+    // find a user based on id
+    findUser = async (id) => {
+        const target_user = await this.#usersModel.findOne({
+            where: {
+                id
+            }
+        })
+        return target_user;
+    }
+    
+    // create a user
+    createUser = async (id, password) => {
+        return await this.#usersModel.create({
+            id,
+            password
+        });
+    }
+  
+}
+
+module.exports = UsersRepository;
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const UsersController = require('../controllers/users.controller');
+const usersController = new UsersController();
+
+router.post('/signup', usersController.signUpUser);
+router.get('/signup/:id', usersController.signUpDuplicateIdCheck)
+router.post('/login', usersController.loginUser);
 
 module.exports = router;
